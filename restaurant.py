5a16c43b0718ad0bd07c18c62d696966cc988a1b
Rafaelorr <rafael.onkelinx@muscardinus.be>
2026-07-13 15:00:33 +0200

    voeg typês toe aan restaurant class

--- restaurant.py
+++ restaurant.py
@@ -1,12 +1,12 @@
 class Restaurant():
     """Een simpele voorstelling van een restaurant"""
 
-    def __init__(self, restaurant_name, cuisine_type):
+    def __init__(self, restaurant_name :str, cuisine_type :str):
         self.restaurant_name = restaurant_name
         self.cuisine_type = cuisine_type
 
     def describe_restaurant(self):
-        print(f"{self.restaurant_name.title()} serveert uit {self.cuisine_type}")
+        print(f"{self.restaurant_name.title()} serveert uit de {self.cuisine_type}")
     
     def open_restaurant(self):
         print(f"{self.restaurant_name.title()} is nu open")
