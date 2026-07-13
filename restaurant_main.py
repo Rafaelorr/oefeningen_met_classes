dfca6d022887cd3dd8812119a1ceae08520ec838
Rafaelorr <rafael.onkelinx@muscardinus.be>
2026-07-13 15:49:26 +0200

    maak restaurant oefening af

---
+++ restaurant_main.py
@@ -0,0 +1,13 @@
+from restaurant import Restaurant
+
+italiaans_restaurant = Restaurant("Italia", "Italiaanse keuken")
+italiaans_restaurant.describe_restaurant()
+italiaans_restaurant.open_restaurant()
+
+pools_restaurant = Restaurant("restaurant polska", "Poolse keuken")
+pools_restaurant.describe_restaurant()
+pools_restaurant.open_restaurant()
+
+japans_restaurant = Restaurant("japans restaurant", "Japanse keuken")
+japans_restaurant.describe_restaurant()
+japans_restaurant.open_restaurant()
