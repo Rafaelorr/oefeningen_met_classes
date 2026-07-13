af835a334675353e1de5cc9a080213693121dfae
Rafaelorr <rafael.onkelinx@muscardinus.be>
2026-07-13 16:41:02 +0200

    voeg login_attemps logica toe aan de User class

--- user.py
+++ user.py
@@ -4,6 +4,7 @@ class User():
     def __init__(self, first_name :str, last_name :str):
         self.first_name = first_name
         self.last_name = last_name
+        self.login_attemps = 0
     
     def greet_user(self):
         print(f"Dag {self.first_name.title()} {self.last_name.title()}.")
@@ -12,3 +13,9 @@ class User():
         print(f"""Gegevens van de gebruiker:
               Voornaam: {self.first_name.title()}
               Achternaam: {self.last_name.title()}""")
+    
+    def increase_login_attemps(self):
+        self.login_attemps += 1
+    
+    def reset_login_attemps(self):
+        self.login_attemps = 0
